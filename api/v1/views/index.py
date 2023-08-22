733ab4d16f83b1d0801252e85081066c74b0b239
chaher13 <chahercrbs@gmail.com>
2023-08-22 13:51:40 +0200

    Add: an new Api
    Co-authored-by: quentinrouger <quentinrouger@users.noreply.github.com>

---
+++ api/v1/views/index.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+from flask import Response
+import json
+from api.v1.views import app_views
+
+
+@app_views.route('/status', strict_slashes=False)
+def return_status():
+    response_data = {"status": "OK"}
+    response = Response(json.dumps(response_data),
+                        content_type='application/json')
+    return response
+
