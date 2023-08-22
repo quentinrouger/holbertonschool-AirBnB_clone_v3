3498fff563f61540415a3808b55f70bcdb057544
chaher13 <chahercrbs@gmail.com>
2023-08-22 15:02:21 +0200

    Add: an error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,19 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask import jsonify
 
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """handler for 404 errors that returns a JSON-formatted 404 status code"""
+    response = jsonify({"error": "Not found"}), 404
+    return response
 
 
 @app.teardown_appcontext
