0111da9380f9a859c23cc3c50cac3c95eac71238
chaher13 <chahercrbs@gmail.com>
2023-08-23 16:42:00 +0200

    update: correct the put method and the post method

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -48,18 +48,18 @@ def create_place(city_id):
     """Create a new place"""
     request_data = request.get_json()
     city = storage.get(City, city_id)
-    user_id = request_data.get("user_id")
-    user = storage.get(User, user_id)
     if city is None:
         return jsonify({'error': 'Place not found'}), 404
-    if user is None:
-        return jsonify({'error': 'User_id not found'}), 404
     if not request_data:
         return jsonify({'error': 'Not a JSON'}), 400
-    if "name" not in request_data:
-        return jsonify({'error': 'Missing name'}), 400
     if "user_id" not in request_data:
         return jsonify({'error': 'Missing user_id'}), 400
+    user_id = request_data.get("user_id")
+    user = storage.get(User, user_id)
+    if user is None:
+        return jsonify({'error': 'User not found'}), 404
+    if "name" not in request_data:
+        return jsonify({'error': 'Missing name'}), 400
 
     new_object = Place(**request_data)
     new_object.city_id = city.id
@@ -68,9 +68,9 @@ def create_place(city_id):
     return jsonify(new_object.to_dict()), 201
 
 
-@app_views.route("/place/<place_id>", methods=["PUT"], strict_slashes=False)
+@app_views.route("/places/<place_id>", methods=["PUT"], strict_slashes=False)
 def update_place(place_id):
-    """Update a city based on its id"""
+    """Update a place based on its id"""
     place_obj = storage.get(Place, place_id)
     if place_obj is None:
         return jsonify({'error': 'Place not Found'}), 404
