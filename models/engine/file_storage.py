680669ce3818c82ac9c6d074888b254bada68947
chaher13 <chahercrbs@gmail.com>
2023-08-24 03:17:37 +0200

    Add: solve the PEP8 of file_storage.py and test_db_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception as e:
             pass
 
     def delete(self, obj=None):
@@ -77,11 +77,13 @@ class FileStorage:
             if obj.id == id:
                 return obj
         return None
-    
+
     def count(self, cls=None):
-        """Returns the number of objects in storage matching the given class name. If no name is passed, returns the count of all objects in storage."""
+        """Returns the number of objects in storage matching
+        the given class name. If no name is passed,
+        returns the count of all objects in storage.
+        """
         if cls is None:
             return len(self.all())
         else:
             return len(self.all(cls))
-        
--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -67,30 +67,34 @@ test_db_storage.py'])
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
 
+
 class TestDBStorage(unittest.TestCase):
     """Test get and count methods."""
     def test_get_method(self):
-            """Tests for get method."""
-            s = State(name="California")
-            s.save()
-            u = User(first_name="Jake", last_name="John", password="jhftyd",
-                    email="exemple@gmail.com")
-            u.save()
-            self.assertIs(s, models.storage.get(State, s.id))
-            self.assertIs(u, models.storage.get(User, u.id))
-            self.assertIs(None, models.storage.get(State, "fake_id"))
-            self.assertIs(None, models.storage.get(User, "fake_id"))
+        """Tests for get method."""
+        s = State(name="California")
+        s.save()
+        u = User(first_name="Jake",
+                 last_name="John",
+                 password="jhftyd",
+                 email="exemple@gmail.com")
+        u.save()
+        self.assertIs(s, models.storage.get(State, s.id))
+        self.assertIs(u, models.storage.get(User, u.id))
+        self.assertIs(None, models.storage.get(State, "fake_id"))
+        self.assertIs(None, models.storage.get(User, "fake_id"))
 
     def test_count_method(self):
         """Tests for count method"""
         self.assertEqual(models.storage.count(State),
-                            len(models.storage.all(State)))
+                         len(models.storage.all(State)))
         self.assertEqual(models.storage.count(User),
-                            len(models.storage.all(User)))
+                         len(models.storage.all(User)))
         current = models.storage.count(State)
         s = State(name="Texas")
         s.save()
         self.assertEqual(models.storage.count(State), current + 1)
+
 
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
