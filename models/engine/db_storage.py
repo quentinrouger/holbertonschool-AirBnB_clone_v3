9dca60824f94fa08baf9de9f46173659a9366406
chaher13 <chahercrbs@gmail.com>
2023-08-24 03:24:47 +0200

    update: set PEP8 of the db_storage and file_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -84,7 +84,7 @@ class DBStorage:
             if obj.id == id:
                 return obj
         return None
-    
+
     def count(self, cls=None):
         """Returns the number of objects in storage matching the given class"""
         if cls is None:
